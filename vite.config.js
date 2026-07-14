fae87f5b0a877fa929920ac1b5dcf0e3727245bf
Karla Andujar <karlaandujar@vt.edu>
2026-07-13 21:44:32 -0400

    Fixing config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default {
+  base:'karlaandujar.github.io'
+}
+
 export default defineConfig({
   plugins: [react()],
 })

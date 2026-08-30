bc0e49e5dea33d14686ca4e121e0788f12f8893f
Karla Andujar <karlaandujar@vt.edu>
2026-08-30 17:38:45 -0400

    Adding trazia project and updating resume

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,6 +17,27 @@ const projects = [
             "/images/sr_cover.png",
             "/images/sr_game.png"
         ]
+    },
+    {
+        title: "Trazia",
+        description: "AI-powered academic planning app that cleanly extracts data from syllabi or course schedules and organizes them into a manageable format",
+        tech: [
+            "React",
+            "TypeScript",
+            "Python",
+            "Vercel",
+            "Supabase",
+            "Render",
+            "FastAPI",
+            "Next.js"
+        ],
+        inspiration: "During my first two years of undergrad, I organized my assignments and exams in an enormous, ever-growing spreadsheet. I found it very useful for tracking deadlines and staying ahead, and eventually, my friends began asking to use my template as well. However, one problem remained constant: manually entering every assignment, exam, deadline, weiht, and other detail was too time-consuming. Trazia automates this process while maintaining the simple, manageable system that made my spreadsheet effective.",
+        github: "https://github.com/karlaandujar/trazia",
+        images: [
+            "/images/tr_cover.png",
+            "/images/tr_dashboard.png",
+            "/images/tr_assignments.png"
+        ]
     }
 ]
 

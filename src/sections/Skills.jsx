4041e7f85a2df852621593c9c0637b50a885748f
Karla Andujar <karlaandujar@vt.edu>
2026-09-22 12:42:27 -0400

    Formatting

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -7,35 +7,26 @@ import "./../styles/skills.css";
 
 
 function Skills(){
-
     return (
-
         <section id="skills">
-
             <h2>Skills</h2>
-
 
             <SkillWindow
                 title="languages"
                 skills={languages}
             />
 
-
             <SkillWindow
                 title="tools"
                 skills={tools}
             />
 
-
             <SkillWindow
                 title="ai_ml"
                 skills={ai}
             />
-
         </section>
-
     );
-
 }
 
 

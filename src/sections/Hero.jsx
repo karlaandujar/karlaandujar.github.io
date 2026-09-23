a38adf239742f9be04ac1fe9dde1e2c9cb9960c3
Karla Andujar <karlaandujar@vt.edu>
2026-09-23 16:31:04 -0400

    Spacing

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,42 +3,32 @@ import "../styles/hero.css";
 function Hero() {
   return (
     <section className="hero">
-
       <p className="terminal">
         &gt; initializing site...
       </p>
-
 
       <h1>
         Hi, I'm Karla.
       </h1>
 
-
       <h2>
         Honors Computer Science Student @ Virginia Tech
       </h2>
 
-
       <div>
-
         <a
             href="/resume.pdf"
             target="_blank"
             rel="noreferrer"
             className="resume-button"
-        >
-            Resume
-        </a>
+        >Resume</a>
 
         <a
             href="https://github.com/karlaandujar"
             target="_blank"
             rel="noreferrer"
             className="github-button"
-        >
-            GitHub
-        </a>
-
+        >GitHub</a>
       </div>
 
     </section>
